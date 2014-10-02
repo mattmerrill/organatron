f28429dfec911e579b927fed92e55e36d1f5fe81
David Smith <bigdave.smith@gmail.com>
2014-10-02 15:08:34 -0700

    Styling of initial page load and beginnings of transition to search results

--- app/assets/javascripts/booking.js
+++ app/assets/javascripts/booking.js
@@ -1,13 +1,38 @@
 var attendees = [];
+var people =  [
+      "David Smith",
+      "Brian Boughton",
+      "Carlos Ochoa",
+      "Jeff Boughton",
+      "Lisa Tassiello",
+      "Matt Merrill",
+      "Sheena Smith",
+      "Todd Meyer"
+    ];
 
-function addAttendee() {
-  console.log("Called");
+function addAttendee(attendee) {
+  attendees.push(attendee);
+  console.log("Current attendees: "+attendees);
+  $('.attendee').last().parents('.form-group').after('<div class="form-group"><div class="col-sm-12"><input class="form-control attendee" placeholder="Add Attendee"></div></div>');
+  $('.attendee').last().focus();
+  $('.attendee').last().autocomplete({
+    source: people
+  });
 }
 
 $(function() {
+  $('.attendee').autocomplete({
+    source: people
+  });
   $(document).on('keypress', '.attendee', function(e) {
     if(e.charCode === 13) {
-      addAttendee();
+      addAttendee($(this).val());
     }
+  });
+
+  $('#Search').on('click', function() {
+    $('#Search').blur().text("Search Again");
+    $('.attendee').fadeOut();
+    $('#Intro').addClass('passive');
   });
 });
